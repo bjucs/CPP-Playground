aab26e6ef357b218fcffccf69df83e97be5b4ce4
bjucs <bju@umd.edu>
2025-01-15 14:37:28 -0500

    feat: adjust Exercise 3.5 to take in a variable amount of scores

--- gradeavg3.cc
+++ gradeavg3.cc
@@ -1,17 +1,36 @@
 #include <iomanip> 
 #include <ios> 
 #include <iostream> 
+#include <sstream> 
 #include <string> 
 #include <vector>
 
 using namespace std;
 
-double calculate_final_grade(vector<double> hw_scores, double midterm_score, double final_score) { 
-    sort(hw_scores.begin(), hw_scores.end());
+// Chapter 4.1 
+double get_median(vector<double> values) { 
+    sort(values.begin(), values.end());
 
-    // Since hw_scores is of fixed size 3 
-    double median = hw_scores[1];
+    typedef vector<double>::size_type vec_sz;
+    vec_sz size = values.size();
+    vec_sz mid = size / 2;
+
+    return size % 2 == 0 ? (values[mid-1] + values[mid])/2:values[mid];
+}
+
+double calculate_final_grade(vector<double> hw_scores, double midterm_score, double final_score) { 
+    double median = get_median(hw_scores);
     return (0.2 * midterm_score) + (0.4 * final_score) + (0.4 * median);
+}
+
+void display_final_output(vector<string> student_names, vector<double> student_final_grades) { 
+    typedef vector<string>::size_type vec_sz;
+    vec_sz size = student_names.size();
+    for (vec_sz i = 0; i < size; i++) { 
+        cout << "Student name: " << student_names[i] << endl;
+        cout << "Student final grade: " << student_final_grades[i] << endl;
+        cout << endl;
+    }
 }
 
 // Exercise 3-5 
@@ -33,14 +52,24 @@ int main() {
             return -1;
         }
 
-        // Fixed number of homeworks (3)
-        cout << "Please enter your 3 homework grades: ";
-        double hw1, hw2, hw3;
-        if (!(cin >> hw1 >> hw2 >> hw3)) { 
-            return -1;
+        vector<double> all_hw; 
+
+        cout << "Please enter your homework grades: ";
+        
+        // Used to ignore leftover input from the buffer 
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        string line;
+        getline(cin, line);
+        /* Used in conjunction with `stream >> hw` to extract all double values
+        from the line of input used for HW scores */ 
+        istringstream stream(line);
+        
+        double hw; 
+        while (stream >> hw) { 
+            all_hw.push_back(hw);
         }
 
-        vector<double> all_hw = {hw1, hw2, hw3};
         double total_score = calculate_final_grade(all_hw, midterm, final);
 
         student_names.push_back(name);
@@ -55,15 +84,8 @@ int main() {
     streamsize prec = cout.precision();
     cout << setprecision(3);
 
-    typedef vector<string>::size_type vec_sz;
-    vec_sz size = student_names.size();
-    for (vec_sz i = 0; i < size; i++) { 
-        cout << "Student name: " << student_names[i] << endl;
-        cout << "Student final grade: " << student_final_grades[i] << endl;
-        cout << endl;
-    }
+    display_final_output(student_names, student_final_grades);
 
     setprecision(prec);
-
     return 0;
 } 
