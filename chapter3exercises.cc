2ae76b452bc276777dca26beed6339ae1446ed0e
bjucs <bju@umd.edu>
2025-01-14 20:31:19 -0500

    feat: implement more Chapter 3 exercises

--- chapter3exercises.cc
+++ chapter3exercises.cc
@@ -3,7 +3,6 @@
 #include <iostream> 
 #include <iterator> 
 
-
 using namespace std;
 
 // Exercises 3.3 -> Get number of distinct strings in vector
@@ -17,9 +16,46 @@ int get_num_distinct_strings(vector<string> strings) {
     return strings.size();
 }
 
+string get_shortest_string(vector<string> strings) { 
+    if (strings.empty()) { 
+        return "";
+    }
+
+    /* `auto` here is a pointer, and the iterator is essentially a pointer
+    of the vector's type */ 
+    auto shortest = strings.begin();
+
+    for (auto it = strings.begin(); it != strings.end(); it++) { 
+        if (it->length() < shortest->length()) { 
+            shortest = it;
+        }
+    }
+
+    return *shortest;
+}
+
+string get_longest_string(vector<string> strings) { 
+    if (strings.empty()) { 
+        return "";
+    }
+
+    auto longest = strings.begin();
+
+    for (auto it = strings.begin(); it != strings.end(); it++) { 
+        if (it->length() > longest->length()) { 
+            longest = it;
+        }
+    }
+
+    return *longest;
+}
+
 int main() { 
     vector<string> strings = {"Brian", "Sydney", "Brian", "Ben", "chr1s"};
+
     cout << "Number of distinct strings: " << get_num_distinct_strings(strings) << endl;
+    cout << "Shortest string: " << get_shortest_string(strings) << endl; 
+    cout << "Longest string: " << get_longest_string(strings) << endl; 
     
     return 0;
 }
